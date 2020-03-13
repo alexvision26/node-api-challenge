2b06f392bc54f880779466cc7561fa5bdb395705
Alex Miller <alexmillerprojects@gmail.com>
2020-03-13 10:25:11 -0700

    finished project and stretch

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const server = express();
 
 server.use(helmet());
 server.use(morgan("dev"));
+server.use(cors());
 server.use(express.json());
 
 const projectsRouter = require("./data/routers/projectsRouter");
